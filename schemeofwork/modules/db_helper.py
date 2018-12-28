5fc8195b3ba3fbb0d3f373cb3ccdf77b21bd8342
daveanson <dave@jazzthecat.co.uk>
2018-12-28 08:07:12 +0000

    test db_topic.get_options and extend testing for db_content.get_options

--- schemeofwork/modules/db_helper.py
+++ schemeofwork/modules/db_helper.py
@@ -3,6 +3,8 @@
 """
 helper routines for retrieving and saving values in the database
 """
+last_sql = ()
+
 def to_db_null(val):
     return "NULL" if val is None else val
 
--- schemeofwork/modules/db_topic.py
+++ schemeofwork/modules/db_topic.py
@@ -1,16 +1,26 @@
 # -*- coding: utf-8 -*-
 from cls_topic import TopicModel
+import db_helper
 
-def get_options(db, topic_id = 0, parent_topic_id = 0):
 
-    str_select = "SELECT DISTINCT id, name, created, created_by FROM view_child_parent_topics WHERE id  = %s or parent_id = %s" % (topic_id, parent_topic_id)
+def get_options(db, topic_id='null', parent_topic_id='null', str_select = ""):
 
-    rows = db.executesql(str_select)
+    str_select = "SELECT DISTINCT id, name, created, created_by FROM view_child_parent_topics WHERE id  = %s or parent_id = %s;" % (db_helper.to_db_null(topic_id), db_helper.to_db_null(parent_topic_id))
 
     data = [];
 
-    for row in rows:
-        model = TopicModel(row[0], row[1], row[2], row[3])
-        data.append(model)
+    try:
+
+        rows = db.executesql(str_select)
+
+        for row in rows:
+            model = TopicModel(row[0], row[1], row[2], row[3])
+            data.append(model)
+
+    except Exception as e:
+        db_helper.last_sql = (str_select, "FAILED")
+        raise Exception("Error getting topics", e)
+
+    db_helper.last_sql = (str_select, "SUCCESS")
 
     return data
