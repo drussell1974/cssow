7e862f7ee936b5ba7644447e239c7c06f311e8d8
daveanson <dave@jazzthecat.co.uk>
2019-01-02 21:06:28 +0000

    create prev and next pages in pagination(close #68)

--- schemeofwork/modules/pager.py
+++ schemeofwork/modules/pager.py
@@ -10,9 +10,41 @@ class Pager:
         self.page_size = int(page_size)
         self.pager_size = int(pager_size)
         self.data = data
+
         ' range of data to display '
+
         self.display_records_start = (self.page-1)*self.page_size
         self.display_records_end = self.display_records_start+self.page_size
+
+
+        ' start page in current group '
+
+        self.start_page_in_current_group = 1
+        self.show_prev_group_of_pages = False
+
+        if self.page > self.pager_size:
+            self.start_page_in_current_group = self.page - (self.page % self.pager_size)
+            self.show_prev_group_of_pages = True
+
+        ' number of pages in current group '
+        total_number_of_records = len(self.data)
+        total_number_of_pages = total_number_of_records / self.page_size + total_number_of_records % self.page_size
+        number_of_pages_remaining = total_number_of_pages - self.start_page_in_current_group
+
+        ' end page in current group '
+
+        if number_of_pages_remaining <= self.pager_size:
+            self.end_page_in_current_group = self.start_page_in_current_group + number_of_pages_remaining
+            self.show_next_group_of_pages = False
+        else:
+            self.end_page_in_current_group = self.start_page_in_current_group + self.pager_size - 1
+            self.show_next_group_of_pages = True
+        import datetime
+        print("******************** {} *******************".format(datetime.datetime.now()))
+        print("self.start_page = {}, self.end_page = {}, self.show_prev = {}, self.show_next = {}, total_number_of_records = {}, total_number_of_pages = {}, number_of_pages_remaining = {}"
+              .format(self.start_page_in_current_group, self.end_page_in_current_group, self.show_prev_group_of_pages, self.show_next_group_of_pages, total_number_of_records, total_number_of_pages, number_of_pages_remaining))
+
+
 
 
     def data_to_display(self):
@@ -27,33 +59,25 @@ class Pager:
         Display e.g only 5 groups of 10 records (1 - 5, 6 - 10, 11 - 12)
         :return: html
         """
-
-        start_page = 1
-        ' start at beginning of the page group '
-        if self.page > self.pager_size:
-            start_page = (self.page - (self.page % self.pager_size)) + 1
-
-        total_number_of_records = len(self.data)
-        remaining = total_number_of_records % (self.pager_size * self.page_size)
-
-        end_page = start_page + self.pager_size
-        if remaining <= end_page:
-            end_page = start_page + int(math.ceil(remaining / self.pager_size))
-
-        print("start {} end {} remaining {}".format(start_page, end_page, remaining))
         html = ""
+        ' show previous group of pages if not in first group and number of pages '
+
+        if self.show_prev_group_of_pages:
+            html = html + create_list_item(url, self.page, self.start_page_in_current_group - 1, "&larr;")
 
         ' create list items '
-        for page_number in range(start_page, end_page):
-            if page_number > 1 and page_number % self.pager_size == 1:
-                ' create previous '
-                html = html + create_list_item(url, self.page, page_number - 1, "&larr;")
+        last_page_number = 1
+        for page_number in range(self.start_page_in_current_group, int(self.end_page_in_current_group) + 1):
+            ' create numbered list item'
 
             html = html + create_list_item(url, self.page, page_number, page_number)
+            last_page_number = page_number
 
-            ' create next '
-            if page_number % self.pager_size == 0:
-                html = html + create_list_item(url, self.page, page_number + 1, "&rarr;")
+
+        ' create next '
+
+        if self.show_next_group_of_pages == True:
+            html = html + create_list_item(url, self.page, page_number + 1, "&rarr;")
 
         return html
 
