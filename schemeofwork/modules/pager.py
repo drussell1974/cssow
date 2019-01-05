a2932ac61bb5e9da39b67f9b213f918676a5d5de
daveanson <dave@jazzthecat.co.uk>
2019-01-05 10:18:27 +0000

    pagination fixed (close #69)

--- schemeofwork/modules/pager.py
+++ schemeofwork/modules/pager.py
@@ -18,9 +18,6 @@ class Pager:
         self.display_records_end = self.display_records_start+self.page_size
 
 
-        #self.set_pagination()
-
-
     def data_to_display(self):
         """
         Displays e.g. only 10 records at a time
@@ -33,6 +30,7 @@ class Pager:
         Display e.g only 5 groups of 10 records (1 - 5, 6 - 10, 11 - 12)
         :return: html
         """
+        self.start_page = 1
 
         if self.page % self.pager_size == 0:
             self.start_page = self.page - 6
@@ -85,95 +83,6 @@ class Pager:
 
 
         return html
-
-
-
-
-    def __render_html(self, url = ""):
-        """
-        Display e.g only 5 groups of 10 records (1 - 5, 6 - 10, 11 - 12)
-        :return: html
-        """
-        html = ""
-
-        if self.show_previous_pages:
-            """ show previous group """
-            html = html + create_list_item(url, self.page, self.start_page, "&larr;")
-
-        display_page_number_as = 0
-        for page_number in range(self.start_page, int(self.end_page)):
-            display_page_number_as = page_number + 1
-            """ create list items """
-            html = html + create_list_item(url, self.page, display_page_number_as, display_page_number_as)
-
-        if self.show_more_pages == True:
-            """ create next """
-            html = html + create_list_item(url, self.page, display_page_number_as + 1, "&rarr;")
-
-        return html
-
-
-    def set_pagination(self):
-        """ ********** """
-        """ pagination """
-        """ ********** """
-        ' number of pages '
-        _total_no_of_records = len(self.data)
-        full_pages = _total_no_of_records // self.page_size
-        trailing_pages1 = _total_no_of_records % self.page_size
-        no_pages = full_pages + trailing_pages1
-        _full_groups = full_pages // self.pager_size
-        pages_in_full_group = _full_groups * self.pager_size
-        trailing_pages2 = no_pages - pages_in_full_group
-        if no_pages <= self.pager_size:
-            self.start_page = 0
-            self.end_page = no_pages
-            self.show_previous_pages = False
-            self.show_more_pages = False
-        else:
-            """ deal with pagination """
-
-            self.show_previous_pages = True
-            self.show_more_pages = True
-
-            if self.page <= self.pager_size:
-                """ first group """
-                print("FIRST GROUP")
-
-                self.start_page = 0
-                self.show_previous_pages = False
-
-                if full_pages < no_pages:
-                    self.end_page = self.pager_size
-                    self.show_more_pages = True
-                else:
-                    self.end_page = self.start_page + trailing_pages1
-                    self.show_more_pages = False
-            else:
-                """ subsequent group """
-
-                self.start_page = self.page - (self.page % self.pager_size)
-                self.end_page = self.start_page + self.pager_size
-                self.show_previous_pages = True
-
-                if self.start_page >= pages_in_full_group:
-                    """ last group """
-                    print("LAST GROUP")
-                    self.start_page = self.page - (self.page % self.pager_size)
-                    self.end_page = self.start_page + trailing_pages2
-                    self.show_more_pages = False
-                    self.show_previous_pages = True
-
-
-        print("_total_no_of_records = {}".format(_total_no_of_records))
-        print("full_pages = {}".format(full_pages))
-        print("trailing_pages1 = {}".format(trailing_pages1))
-        print("no_pages = {}".format(no_pages))
-        print("**********************************************")
-        print("_full_groups = {}".format(_full_groups))
-        print("pages_in_full_group = {}".format(pages_in_full_group))
-        print("trailing_pages2 = {}".format(trailing_pages2))
-        print("**********************************************")
 
 
 #list_item = "<li><a href='{{=URL('schemesofwork', 'index', vars=dict(page=1))}}' class='btn {{if page == 1:}} btn-primary {{ pass }}'>1</a></li>"
