31b3392b4bf2f392a246afb5d42a46aa0895241e
daveanson <dave@jazzthecat.co.uk>
2019-01-21 15:03:56 +0000

    group in learning objectives

--- schemeofwork/controllers/learningobjective.py
+++ schemeofwork/controllers/learningobjective.py
@@ -26,24 +26,9 @@ def index():
     learning_episode = db_learningepisode.get_model(db, learning_episode_id, auth.user_id)
     data = db_learningobjective.get_all(db, learning_episode_id, auth.user_id)
 
-    # bubble sort by solo
-    sorted_data = data
+    # bubble sort by solo and group name
 
-    while True:
-        swapped = False
-        for i in range(len(sorted_data)-1):
-            if sorted_data[i].solo_taxonomy_level > sorted_data[i+1].solo_taxonomy_level:
-                """ put item in the correct position """
-                temp1 = sorted_data[i]
-                temp2 = sorted_data[i+1]
-
-                sorted_data[i] = temp2
-                sorted_data[i+1] = temp1
-                swapped = True
-
-        if swapped == False:
-            """ no more sorting required so finish """
-            break
+    sorted_data = _sort_by_solo_and_group(data)
 
     # page the data
     pager = Pager(page = page_to_display, page_size = 10, data = sorted_data)
@@ -69,6 +54,44 @@ def index():
         learningepisiode_options = learning_episode_options, #TODO: create view_learningepisiode_options: remove this line
         page = page_to_display,
         pager_html = pager_html)
+
+
+def _sort_by_solo_and_group(data):
+    sorted_data = data
+
+    while True:
+        swapped = False
+        for i in range(len(sorted_data) - 1):
+            if sorted_data[i].solo_taxonomy_level > sorted_data[i + 1].solo_taxonomy_level:
+                """ put item in the correct position """
+                temp1 = sorted_data[i]
+                temp2 = sorted_data[i + 1]
+
+                sorted_data[i] = temp2
+                sorted_data[i + 1] = temp1
+                swapped = True
+
+        if swapped == False:
+            """ no more sorting required so finish """
+            break
+    # bubble sort by group_name
+    while True:
+        swapped = False
+        for i in range(len(sorted_data) - 1):
+            if sorted_data[i].group_name > sorted_data[i + 1].group_name:
+                """ put item in the correct position """
+                temp1 = sorted_data[i]
+                temp2 = sorted_data[i + 1]
+
+                sorted_data[i] = temp2
+                sorted_data[i + 1] = temp1
+                swapped = True
+
+        if swapped == False:
+            """ no more sorting required so finish """
+            break
+
+    return sorted_data
 
 
 @auth.requires_login()
