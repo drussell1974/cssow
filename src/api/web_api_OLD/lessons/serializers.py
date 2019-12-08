5153b87f872939ec6bff06832d30888851e8c5e8
Dave <dave@daverussell.co.uk>
2019-12-08 06:29:28 +0000

    remove old web_api folder

--- src/api/web_api_OLD/lessons/serializers.py
+++
@@ -1,9 +0,0 @@
-from schemeofwork.modules.cls_learningepisode import LearningEpisodeModel
-from rest_framework import serializers
-
-class LessonSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        models = LearningEpisodeModel(id_=0, title="")
-        fields = ["title", "summary"]
-
-    
