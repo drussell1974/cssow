894e85ce707821db5feb6c92ae0a9e54e5045cdc
Dave <dave@daverussell.co.uk>
2019-12-21 02:33:40 +0000

    split project into workspaces

--- src/api/web_api/manage.py
+++ src/api/web_api/manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-sys.path.append('../../../../modules')
+sys.path.append('../../modules/')
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_api.settings')
