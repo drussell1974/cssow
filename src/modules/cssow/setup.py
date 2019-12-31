e9514d01a91c7f48f7ac6b1eab9b36cc1a644e96
Dave <dave@daverussell.co.uk>
2019-12-31 07:10:57 +0000

    Create cssow package

---
+++ src/modules/cssow/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cssow-drussell1974", # Replace with your own username
+    version="0.0.1",
+    author="Dave Russell",
+    author_email="dave@daverussell.co.uk",
+    description="Computer Science Scheme of work",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/drussell1974/schemeofwork_web2py_app",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
