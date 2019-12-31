d1b9f07edd27615b051387b18013a72a2ba59999
Dave <dave@daverussell.co.uk>
2019-12-31 07:09:23 +0000

    Create default

--- src/teacher_web/web/default/views.py
+++ src/teacher_web/web/default/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+import models.cls_schemeofwork
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, World")
+
+    # get the schemes of work
+    latest_schemes_of_work = cls_schemeofwork.get_latest_schemes_of_work(db, top = 5, auth_user = auth.user_id)
+
+    content = {
+        "main_heading":"Teach Computer Science",
+        "sub_heading":"Computing Schemes of Work across all key stages"
+              }
+    context = {
+        "page_title": "Dave Russell - Teach Computer Science",
+        "content": content,
+        "latest_schemes_of_work": latest_schemes_of_work
+    }
+
+    return render(request, "default/index.html", context)
--- src/teacher_web/web/web/settings.py
+++ src/teacher_web/web/web/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = ["admin.daverussell.local"]
 # Application definition
 
 INSTALLED_APPS = [
+    'default',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
