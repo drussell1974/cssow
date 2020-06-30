ed43efe40f372a3b1828303715ef40081c3c2fdd
drussell1974 <dave@jazzthecat.co.uk>
2020-06-30 15:27:22 +0100

    create docker-compose for build

--- docker-build--all.sh
+++ docker-build--all.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo "docker-build.sh: creating docker-compose.yml.build..."
+
+sed '/build:/d' docker-compose.yml > docker-compose.yml.build
+
 
 echo "docker-build.sh: creating build..."
 
@@ -6,6 +10,7 @@ sh docker-build--cssow-db.sh
 sh docker-build--teacher-web.sh
 sh docker-build--markdown-service.sh
 sh docker-build--student-web.sh
+
 
 echo "docker-build.sh: running docker compose build and up"
 
