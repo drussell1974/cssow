3aff6e1359e68aab6ad8463766230c6df420f664
drussell1974 <dave@jazzthecat.co.uk>
2020-07-02 05:21:53 +0100

    remove redundant files

--- docker/teacher-web/build--teacher-web.sh
+++ docker/teacher-web/build--teacher-web.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
 # install cssow models
+echo "build-teacher-web.sh: installing cssow models dist/cssow_drussell1974-$CSSOWMODEL_APP__VERSION-py3-none-any.whl"
+
 cd build
 cd cssow
+
 python setup.py sdist bdist_wheel
-cd dist
-pip install cssow_drussell1974-$CSSOWMODEL_APP__VERSION-py3-none-any.whl
+pip install dist/cssow_drussell1974-$CSSOWMODEL_APP__VERSION-py3-none-any.whl
+
 
 # install python pip requirements
-cd ..
+echo "build-teacher-web.sh: installing python requirements from requirements.txt"
+
 cd ..
 cd teacher_web
-pip install requirements.txt
+
+pip install -r requirements.txt
+
 
 # run web server
+echo "build-teacher-web.sh: django 'python manage.py runserver $TEACHER_WEB__WEB_SERVER_IP:$TEACHER_WEB__WEB_SERVER_PORT_INT'"
 
 cd web
+
 python manage.py runserver $TEACHER_WEB__WEB_SERVER_IP:$TEACHER_WEB__WEB_SERVER_PORT_INT
