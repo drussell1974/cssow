97c3ff736da414fe1a15dd519853dd03c456ea22
drussell1974 <dave@jazzthecat.co.uk>
2020-07-05 17:01:31 +0100

    Update build models now in teacher-web project

--- docker/teacher-web/build--teacher-web.sh
+++ docker/teacher-web/build--teacher-web.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
-
-# install cssow models
-echo "build-teacher-web.sh: installing cssow models dist/cssow_drussell1974-$CSSOWMODEL_APP__VERSION-py3-none-any.whl"
-
-cd build
-cd cssow
-
-python setup.py sdist bdist_wheel
-pip install dist/cssow_drussell1974-$CSSOWMODEL_APP__VERSION-py3-none-any.whl
-
-
-# install python pip requirements
-echo "build-teacher-web.sh: installing python requirements from requirements.txt"
-
-cd ..
-cd teacher_web
-
-pip install -r requirements.txt
-
-
-# run web server
 echo "build-teacher-web.sh: django 'python manage.py runserver $TEACHER_WEB__WEB_SERVER_IP:$TEACHER_WEB__WEB_SERVER_PORT_INT'"
 
-cd web
-
-python manage.py runserver $TEACHER_WEB__WEB_SERVER_IP:$TEACHER_WEB__WEB_SERVER_PORT_INT
+cd build
+python manage.py runserver --settings=web.settings.production.settings  $TEACHER_WEB__WEB_SERVER_IP:$TEACHER_WEB__WEB_SERVER_PORT_INT 
