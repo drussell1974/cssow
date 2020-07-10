ddd6c18a428cd72f8f1538ac69fe348e8334e783
drussell1974 <dave@jazzthecat.co.uk>
2020-07-10 09:41:46 +0100

    build and push during build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,26 @@
-echo \"creating docker-compose.yml and .env examples...\"
-sed /build/d docker-compose.yml > build/docker-compose.yml
-cp .env build/.env
-tar -czvf releases/release-$(date +%F).tar.gz ./build 
-git add releases/release-$(date +%F).tar.gz
-git commit -m 'build: release'
+## Run project tasks 
 
-yarn release
+sudo cp -r ~/db/backups/db-backup.cssow_api.latest.sql build/cssow-db/db/current
+
+BUILDNO=$(date +%F-%H%M)
+
+echo Creating release-$BUILDNO...
+
+yarn --cwd src/student_web build
+yarn --cwd src/markdown-service build
+yarn --cwd src/teacher_web build
+
+# Create docker-compose with new build number
+sed "s/BUILDNO/$BUILDNO/"  build/docker-compose.TEMPLATE.yml > build/docker-compose.yml
+sudo docker-compose -f build/docker-compose.yml build
+sudo docker-compose -f build/docker-compose.yml push
+
+## create tar releases
+
+# Remove build
+sed "/build:/d" -i build/docker-compose.yml
+
+cp .env build/.env
+tar -czvf releases/release-$BUILDNO.tar.gz ./build 
+git add releases/release-$BUILDNO.tar.gz
+git commit -m "build: release-$BUILDNO"
--- scripts/release.sh
+++
@@ -1,3 +0,0 @@
-sudo cp -r ~/db/backups/db-backup.cssow_api.latest.sql build/cssow-db/db/current
-yarn --cwd src/student_web build
-yarn --cwd src/markdown-service build && yarn --cwd src/teacher_web build
