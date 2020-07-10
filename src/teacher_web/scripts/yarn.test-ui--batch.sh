8edbd8aee6b8678873015c830c68086a06455723
drussell1974 <dave@jazzthecat.co.uk>
2020-07-10 08:29:58 +0100

    build: release

--- src/teacher_web/scripts/yarn.test-ui--batch.sh
+++ src/teacher_web/scripts/yarn.test-ui--batch.sh
@@ -9,7 +9,8 @@ export TEST_USER_NAME="test@localhost"
 export TEST_USER_PSWD="co2m1c1."
 
 echo -e "\e[1;33m Use virtualenv 'source .python3-env/bin/activate' and run pip install -r requirements \e[0m"
-echo -e "\e[1;33m Ensure task build:test-ui is running and http://${TEST_HOST}:${TEST_PORT} is available \e[0m"
+echo -e "\e[1;33m Run docker-compose up --build cssow-db \e[0m"
+echo -e "\e[1;33m Run task build:test-ui and ensure web server is running and http://${TEST_HOST}:${TEST_PORT} is available \e[0m"
 
 echo Testing... default
 python -m unittest discover --start-directory ./tests/ui_test/ -p uitest_schemeofwork_default_*.py
