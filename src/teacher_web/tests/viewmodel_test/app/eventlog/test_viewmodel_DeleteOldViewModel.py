ea5a1df3a3a3b4525e6edd579bdc0b1b590d18d8
drussell1974 <dave@jazzthecat.co.uk>
2020-08-24 18:18:28 +0100

    fix settings-label (close #274)

---
+++ src/teacher_web/tests/viewmodel_test/app/eventlog/test_viewmodel_DeleteOldViewModel.py
@@ -0,0 +1,155 @@
+import json
+from unittest import TestCase, skip
+from unittest.mock import MagicMock, Mock, PropertyMock, patch
+
+# test context
+
+from app.eventlogs.viewmodels import EventLogDeleteOldViewModel as ViewModel
+from shared.models.cls_eventlog import EventLogModel as Model
+
+
+class fake_settings:
+    MIN_NUMBER_OF_DAYS_TO_KEEP_LOGS = 7
+    
+class test_viewmodel_DeleteOldViewModel(TestCase):
+
+
+    def setUp(self):    
+        self.fake_settings = fake_settings()
+
+        pass
+        
+
+    def tearDown(self):
+        pass
+
+
+    def test_init_called_delete__with_exception(self):
+        
+        # arrange        
+        with patch.object(Model, "delete", side_effect=KeyError):
+
+            db = MagicMock()
+            db.cursor = MagicMock()
+
+            mock_request = Mock()
+            mock_request.method = "POST"
+            mock_request.POST = { "days": 30 }
+
+            self.mock_model = Mock()
+
+            with self.assertRaises(KeyError):
+                # act
+                self.viewmodel = ViewModel(db, mock_request, self.fake_settings, auth_user=6079)
+
+
+    def test_init_called_delete__should_not_call_delete_when_GET_request(self):
+        
+        # arrange        
+        with patch.object(Model, "delete"):
+
+            db = MagicMock()
+            db.cursor = MagicMock()
+
+            mock_request = Mock()
+            mock_request.method = "GET"
+            mock_request.POST = { "days": 30 }
+
+            # act
+            self.viewmodel = ViewModel(db, mock_request, self.fake_settings, auth_user=6079)
+
+            # assert
+            Model.delete.assert_not_called()
+
+
+    def test_init_called_delete__should_not_call_delete_when_days_less_than_7(self):
+        
+        # arrange        
+        with patch.object(Model, "delete"):
+
+            db = MagicMock()
+            db.cursor = MagicMock()
+
+            mock_request = Mock()
+            mock_request.method = "POST"
+            mock_request.POST = { "days": 6 }
+            
+            
+            with self.assertRaises(Exception):
+                # act
+                self.viewmodel = ViewModel(db, mock_request, self.fake_settings, auth_user=6079)
+
+            # assert
+            Model.delete.assert_not_called()
+
+
+    def test_init_called_delete__no_return_rows(self):
+        
+        # arrange
+        
+        data_to_return = None
+        
+        with patch.object(Model, "delete", return_value=data_to_return):
+
+            db = MagicMock()
+            db.cursor = MagicMock()
+
+            mock_request = Mock()
+            mock_request.method = "POST"
+            mock_request.POST = { "days": 7 }
+
+            self.mock_model = Mock()
+
+            # act
+            self.viewmodel = ViewModel(db, mock_request, self.fake_settings, auth_user=6079)
+
+            # assert functions was called
+            Model.delete.assert_called_with(db, 7, 6079)
+
+            self.assertIsNone(self.viewmodel.model)
+
+
+    def test_init_called_delete__return_item(self):
+        
+        # arrange
+        
+        with patch.object(Model, "delete", return_value=1):
+
+            db = MagicMock()
+            db.cursor = MagicMock()
+
+            mock_request = Mock()
+            mock_request.method = "POST"
+            mock_request.POST = { "days": 14 }
+            
+            self.mock_model = Mock()
+
+            # act
+            self.viewmodel = ViewModel(db, mock_request, self.fake_settings, auth_user=6079)
+
+            # assert functions was called
+            Model.delete.assert_called_with(db, 14, 6079)
+            self.assertEqual(1, self.viewmodel.model)
+
+
+    def test_init_called_delete__return_items(self):
+        
+        # arrange
+        
+        with patch.object(Model, "delete", return_value=3):
+
+            db = MagicMock()
+            db.cursor = MagicMock()
+
+            mock_request = Mock()
+            mock_request.method = "POST"
+            mock_request.POST = { "days": 28 }
+
+            self.mock_model = Mock()
+
+            # act
+            self.viewmodel = ViewModel(db, mock_request, self.fake_settings, auth_user=6079)
+
+            # assert functions was called
+            Model.delete.assert_called_with(db, 28, 6079)
+            self.assertEqual(3, self.viewmodel.model)
