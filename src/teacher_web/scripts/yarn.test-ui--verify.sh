367a77247a6fdf111b07a7c83da4ae08fd7ee3ab
drussell1974 <dave@jazzthecat.co.uk>
2020-12-28 15:19:07 +0000

    create tests for keyword index using javascript (closes #304)

--- src/teacher_web/scripts/yarn.test-ui--verify.sh
+++ src/teacher_web/scripts/yarn.test-ui--verify.sh
@@ -1,5 +1,5 @@
 ## start test
 echo yarn.test-ui--batch.sh: Testing... verify by file name - use asterisks wildcard as neccessary
-python3 -m unittest discover --start-directory ./tests/ui_test/ -p uitest_schemeofwork_resources_*.py
+python3 -m unittest discover --start-directory ./tests/ui_test/ -p uitest_schemeofwork_keyword_*_index__search_term.py
 
 exit $x
---
+++ src/teacher_web/tests/ui_test/uitest_schemeofwork_keyword_index__search_term.py
@@ -0,0 +1,66 @@
+from ui_testcase import UITestCase, WebBrowserContext
+
+class uitest_schemeofwork_keyword_index__search_term(UITestCase):
+
+    test_context = WebBrowserContext()
+
+    def setUp(self):
+        # set up
+        self.test_path = "/schemesofwork/{}/keywords".format(self.test_scheme_of_work_id)
+        self.test_context.get(self.root_uri + self.test_path)
+
+        self.test_context.implicitly_wait(4)
+
+
+    def tearDown(self):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        # tear down
+        cls.test_context.close()
+
+
+    def test_page__should_all_by_default(self):
+        # test
+        elem = self.test_context.find_elements_by_xpath("//*[contains(@class, 'card-keyword')]")
+        
+        # assert
+        self.assertEqual(193, len(elem))
+
+
+    def test_page__should_show_none(self):
+        
+        # test
+        elem = self.test_context.find_element_by_id("ctl-keyword_search")
+        elem.send_keys("abcd")
+        
+        elem = self.test_context.find_elements_by_xpath("//*[contains(@style, 'block')]")
+        
+        # assert
+        self.assertEqual(0, len(elem))
+
+
+    def test_page__should_upper_and_lower_case_matches(self):
+        
+        # test
+        elem = self.test_context.find_element_by_id("ctl-keyword_search")
+        elem.send_keys("Ram")
+        self.wait()
+
+        elem = self.test_context.find_elements_by_xpath("//*[contains(@style, 'block')]")
+        
+        # assert
+        self.assertEqual(2, len(elem))
+
+
+    def test_page__should_single_item_match(self):
+        
+        # test
+        elem = self.test_context.find_element_by_id("ctl-keyword_search")
+        elem.send_keys("Random Access Memory (RAM)")
+        
+        elem = self.test_context.find_elements_by_xpath("//*[contains(@style, 'block')]")
+        
+        # assert
+        self.assertEqual(1, len(elem))
