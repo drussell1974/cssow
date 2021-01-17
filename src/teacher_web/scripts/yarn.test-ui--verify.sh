0cec94f872b298989608ce5a86c9d7bcb05c1998
drussell1974 <dave@jazzthecat.co.uk>
2021-01-17 13:24:25 +0000

    test permissions on lesson views - add copy test (#206)

--- src/teacher_web/scripts/yarn.test-ui--verify.sh
+++ src/teacher_web/scripts/yarn.test-ui--verify.sh
@@ -1,7 +1,6 @@
 ## start test
 echo yarn test-ui--verify.sh: Testing... verify by file name - use asterisks wildcard as neccessary
 
-
 #python -m unittest discover --start-directory ./tests/ui_test/ -p uitest_registration_password_*.py
 
 #python -m unittest discover --start-directory ./tests/ui_test/ -p uitest_schemeofwork_default_*.py
@@ -13,7 +12,7 @@ echo yarn test-ui--verify.sh: Testing... verify by file name - use asterisks wil
 
 #python -m unittest discover --start-directory ./tests/ui_test/ -p uitest_schemeofwork_content_*.py
 
-#python -m unittest discover --start-directory ./tests/ui_test/ -p uitest_schemeofwork_lesson_*.py
+python -m unittest discover --start-directory ./tests/ui_test/ -p uitest_schemeofwork_lesson_*.py
 python -m unittest discover --start-directory ./tests/ui_test/ -p uitest_schemeofwork_lesson_pages__public_permissions.py
 
 #python -m unittest discover --start-directory ./tests/ui_test/ -p uitest_schemeofwork_learningobjective_*.py
--- src/teacher_web/tests/ui_test/uitest_schemeofwork_lesson_pages__public_permissions.py
+++ src/teacher_web/tests/ui_test/uitest_schemeofwork_lesson_pages__public_permissions.py
@@ -21,6 +21,8 @@ class uitest_schemeofwork_lesson_pages__public_permissions(UITestCase):
 
 
     def test_page__index__should_redirect_to_login_when_with_permission_error(self):
+        #path('', views.index, name="lesson.index"),
+        #path('<int:lesson_id>', views.index, name="lesson.view"),
         # test
         self.try_log_out(f"/schemesofwork/{self.test_scheme_of_work_id}/lessons")
         
@@ -29,6 +31,7 @@ class uitest_schemeofwork_lesson_pages__public_permissions(UITestCase):
 
 
     def test_page__new__should_redirect_to_login_when_with_permission_error(self):
+        #path('new', views.edit, name="lesson.new"),    
         # test
         self.try_log_out(f"/schemesofwork/{self.test_scheme_of_work_id}/lessons/new")
         
@@ -37,6 +40,7 @@ class uitest_schemeofwork_lesson_pages__public_permissions(UITestCase):
 
 
     def test_page__edit__should_redirect_to_login_when_with_permission_error(self):
+        #path('<int:lesson_id>/edit', views.edit, name="lesson.edit"),
         # test
         self.try_log_out(f"/schemesofwork/{self.test_scheme_of_work_id}/lessons/{self.test_lesson_id}/edit")
         
@@ -44,7 +48,17 @@ class uitest_schemeofwork_lesson_pages__public_permissions(UITestCase):
         self.assertLoginPage(login_message="Enter your email and password", exception_message="PermissionError at /schemesofwork/11/lessons/220/edit")
 
 
+    def test_page__copy__should_redirect_to_login_when_with_permission_error(self):
+        #path('<int:lesson_id>/copy', views.edit, { "is_copy": True }, name="lesson.copy"), 
+        # test
+        self.try_log_out(f"/schemesofwork/{self.test_scheme_of_work_id}/lessons/{self.test_lesson_id}/copy")
+        
+        # assert
+        self.assertLoginPage(login_message="Enter your email and password", exception_message="PermissionError at /schemesofwork/11/lessons/220/copy")
+
+
     def test_page__publish__should_redirect_to_login_when_with_permission_error(self):
+        #path('<int:lesson_id>/publish', views.publish, name="lesson.publish_item"), 
         # test
         self.try_log_out(f"/schemesofwork/{self.test_scheme_of_work_id}/lessons/{self.test_lesson_id}/publish")
         
@@ -53,6 +67,7 @@ class uitest_schemeofwork_lesson_pages__public_permissions(UITestCase):
 
 
     def test_page__whiteboard__should_be_allowed(self):
+        #path('<int:lesson_id>/whiteboard', views.whiteboard, name="lesson.whiteboard_view"),
         # test
         self.try_log_out(f"/schemesofwork/{self.test_scheme_of_work_id}/lessons/{self.test_lesson_id}/whiteboard")
         
@@ -61,9 +76,9 @@ class uitest_schemeofwork_lesson_pages__public_permissions(UITestCase):
 
 
     def test_page__delete_unpublished__should_redirect_to_login_when_with_permission_error(self):
+        #path('delete_unpublished', views.delete_unpublished, name="lesson.delete_unpublished"), 
         # test
         self.try_log_out(f"/schemesofwork/{self.test_scheme_of_work_id}/lessons/delete_unpublished")
         
         # assert
         self.assertLoginPage(login_message="Enter your email and password", exception_message="PermissionError at /schemesofwork/delete_unpublished")
-
