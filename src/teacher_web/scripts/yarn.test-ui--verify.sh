ed781a06a8e8a46ea6c272cf818d06808863bb61
drussell1974 <dave@jazzthecat.co.uk>
2021-01-23 19:05:21 +0000

    fix other schemes of work sidebar

--- src/teacher_web/scripts/yarn.test-ui--verify.sh
+++ src/teacher_web/scripts/yarn.test-ui--verify.sh
@@ -18,7 +18,7 @@ echo "yarn.test-ui--verify.sh:\e[1;33m Run 'fuser -k 3002/tcp' to kill exiting p
 
 #python -m unittest discover --start-directory ./tests/ui_test/ -p uitest_schemeofwork_content_edit_create_existing.py
 
-python -m unittest discover --start-directory ./tests/ui_test/ -p uitest_permissions_schemeofwork_content__when_different_logged_in_users.py
+python -m unittest discover --start-directory ./tests/ui_test/ -p uitest_schemeofwork_schemesofworkkeyword_index.py
 
 #python -m unittest discover --start-directory ./tests/ui_test/ -p uitest_schemeofwork_lessonkeyword_*.py
 
