27f58551750c95d11e98b7b31890c9e0b1d72c8c
drussell1974 <dave@jazzthecat.co.uk>
2021-02-28 15:56:11 +0000

    sql update for release

--- src/database/schema/storedprocedures/sql_update.sql
+++ src/database/schema/storedprocedures/sql_update.sql
@@ -557,6 +557,14 @@ BEGIN
 END;
 //
 
+SELECT id FROM sow_key_word
+			WHERE scheme_of_work_id = 11 
+					AND published IN (32,64) 
+					AND 2 IN 
+							(SELECT auth_user_id 
+							FROM sow_teacher 
+							WHERE scheme_of_work_id = 11);
+
 DELIMITER ;DELIMITER //
 
 DROP PROCEDURE IF EXISTS keyword__get;
@@ -727,12 +735,14 @@ CREATE PROCEDURE keyword__delete_unpublished (
  IN p_auth_user INT)
 BEGIN
     DELETE FROM sow_key_word
+	-- UPDATE sow_key_word
+    -- SET published = 64
     WHERE scheme_of_work_id = p_scheme_of_work_id 
-            AND published IN (32,64) 
-            AND p_auth_user IN 
-                    (SELECT auth_user_id 
-                    FROM sow_teacher 
-                    WHERE scheme_of_work_id = p_scheme_of_work_id);
+            AND published IN (32,64)
+			AND p_auth_user IN 
+					(SELECT auth_user_id 
+					FROM sow_teacher 
+					WHERE scheme_of_work_id = p_scheme_of_work_id);
 
 END;
 //
@@ -1054,6 +1064,36 @@ BEGIN
 		);
 END;
 
+//
+
+DELIMITER ;DELIMITER //
+
+DROP PROCEDURE IF EXISTS keyword__delete;
+
+CREATE PROCEDURE keyword__delete (
+ IN p_keyword_id INT,
+ IN p_scheme_of_work_id INT,
+ IN p_auth_user INT)
+BEGIN
+    INSERT INTO sow_key_word__archive (id, name, definition, scheme_of_work_id)
+    SELECT id, name, definition, scheme_of_work_id
+    FROM sow_key_word
+    WHERE id = p_keyword_id and scheme_of_work_id = p_scheme_of_work_id;
+
+    INSERT INTO sow_lesson__has__key_words__archive (key_word_id, lesson_id)
+    SELECT lkw.key_word_id, lkw.lesson_id
+    FROM sow_key_word kw 
+        INNER JOIN sow_lesson__has__key_words lkw ON lkw.key_word_id = kw.id
+        WHERE kw.id = p_keyword_id and kw.scheme_of_work_id = p_scheme_of_work_id;
+
+    DELETE FROM sow_key_word
+    -- UPDATE sow_key_word
+    -- SET published = 64
+    WHERE id = p_keyword_id and scheme_of_work_id = p_scheme_of_work_id
+        AND p_auth_user IN (SELECT auth_user_id 
+                            FROM sow_teacher 
+                            WHERE auth_user_id = p_auth_user AND scheme_of_work_id = p_scheme_of_work_id);
+END;
 //
 
 DELIMITER ;DELIMITER //
