9873b3811bba2f4d51b8c29b1e9fb17bab599f4b
drussell1974 <dave@jazzthecat.co.uk>
2021-03-02 07:07:39 +0000

    fix broken student portal link (#345

--- src/teacher_web/web/shared/models/utils/tags.py
+++ src/teacher_web/web/shared/models/utils/tags.py
@@ -4,13 +4,16 @@ from django import template
 register = template.Library()
 
 @register.simple_tag
-def student_uri(scheme_of_work_id = None, lesson_id = None):
+def student_uri(institute_id, department_id, scheme_of_work_id = None, lesson_id = None):
     base_uri = settings.STUDENT_WEB__WEB_SERVER_WWW
 
+    base_uri = base_uri + "/institute/%s" % institute_id
+    base_uri = base_uri + "/department/%s/course/" % department_id
+    
     if scheme_of_work_id is not None:
-        base_uri = base_uri + "/course/%s" % scheme_of_work_id
+        base_uri = base_uri + "%s/lesson/" % scheme_of_work_id
 
     if lesson_id is not None:
-        base_uri = base_uri + "/lesson/%s" % lesson_id
+        base_uri = base_uri + "%s" % lesson_id
 
     return base_uri
