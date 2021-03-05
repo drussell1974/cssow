3f772bb03a65cb644b725485a89e7e438591fd6c
drussell1974 <dave@jazzthecat.co.uk>
2021-03-05 04:33:55 +0000

    update sql

--- src/database/schema/tables/alter_table__sow_key_word.sql
+++ src/database/schema/tables/alter_table__sow_key_word.sql
@@ -3,6 +3,9 @@ SET SQL_SAFE_UPDATES=0;
 DELETE FROM sow_lesson__has__key_words WHERE key_word_id NOT IN (SELECT id FROM sow_key_word);
 SET SQL_SAFE_UPDATES=1;
 
+ALTER TABLE `sow_key_word` 
+MODIFY COLUMN created DATETIME DEFAULT CURRENT_TIMESTAMP;
+
 ALTER TABLE `sow_lesson__has__key_words` 
 ADD CONSTRAINT `fk_sow_lesson__has__key_words__has__keyword`
   FOREIGN KEY (`key_word_id`)
