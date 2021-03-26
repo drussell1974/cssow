0645d0efabe087f2a3ce1ba0a2363857f3dd1963
drussell1974 <dave@jazzthecat.co.uk>
2021-03-26 03:39:07 +0000

    add whiteboard button to lesson item on lesson index page and add other options to dropdown (#411)

--- src/teacher_web/scripts/yarn.test-ui--verify.sh
+++ src/teacher_web/scripts/yarn.test-ui--verify.sh
@@ -9,8 +9,8 @@ echo "yarn.test-ui--verify.sh:\e[1;33m Run 'fuser -k 3002/tcp' to kill exiting p
 echo "running..."
 
 ## start test
-python -m unittest discover --start-directory ./tests/ui_test/ -p uitest_accounts_index.py
-python -m unittest discover --start-directory ./tests/ui_test/ -p uitest_schemeofwork_default_index.py
+python -m unittest discover --start-directory ./tests/ui_test/ -p uitest_schemeofwork_lesson_index.py
+#python -m unittest discover --start-directory ./tests/ui_test/ -p uitest_schemeofwork_default_index.py
 #python -m unittest discover --start-directory ./tests/ui_test/ -p uitest_schemeofwork_resources_edit_delete.py
 #python -m unittest discover --start-directory ./tests/ui_test/ -p uitest_schemeofwork_resources_edit_cancel.py
 #python -m unittest discover --start-directory ./tests/ui_test/ -p uitest_schemeofwork_resources_edit_existing.py
