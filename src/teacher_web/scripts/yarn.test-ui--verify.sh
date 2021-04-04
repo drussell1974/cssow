491fcad65d40a8fd237019bf4c94a381ff812f39
drussell1974 <dave@jazzthecat.co.uk>
2021-04-04 02:55:16 +0100

    show whiteboard per lesson schedule (#432)

--- src/teacher_web/scripts/yarn.test-ui--verify.sh
+++ src/teacher_web/scripts/yarn.test-ui--verify.sh
@@ -10,9 +10,7 @@ echo "running..."
 
 ### start test
 
-python -m unittest discover --start-directory ./tests/ui_test/ -p uitest_schemeofwork_learningobjective_index.py
-python -m unittest discover --start-directory ./tests/ui_test/ -p uitest_schemeofwork_lessonkeyword_index.py
-python -m unittest discover --start-directory ./tests/ui_test/ -p uitest_schemeofwork_resources_index.py
+python -m unittest discover --start-directory ./tests/ui_test/ -p uitest_permissions_schemeofwork_lesson__when_different_logged_in_users.py
 
 #python -m unittest discover --start-directory ./tests/ui_test/ -p uitest_institute_*.py
 #python -m unittest discover --start-directory ./tests/ui_test/ -p uitest_department_*.py
