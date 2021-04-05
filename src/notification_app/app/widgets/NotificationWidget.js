a2de506a8579d2c4c3ff17a108d2c3ff5b22c413
drussell1974 <dave@jazzthecat.co.uk>
2021-04-05 19:49:46 +0100

    move notification to header (#358)

--- src/notification_app/app/widgets/NotificationWidget.js
+++ src/notification_app/app/widgets/NotificationWidget.js
@@ -7,7 +7,7 @@ const NotificationWidget = ({messages, deleteMessageCallback}) => {
                 <button className="badge badge-info dropdown-toggle" type="button" id="dropdownNotificationsMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i className="fa fa-inbox"></i> {Object.keys(messages).length}</button>
                 <div className="dropdown-menu" aria-labelledby="dropdownNotificationsMenuButton">
                     {Object.keys(messages).map(id => (
-                        <div key={id} className="dropdown-item alert alert-warning alert-dismissible fade show" role="alert">
+                        <div key={id} className="dropdown-item">
                             <strong>{messages[id].message}</strong> <a className="dropdown-link" href={messages[id].action}>action now</a> <button onClick={deleteMessageCallback.bind(this, id)} type="button" className="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                         </div>
                     ))}
