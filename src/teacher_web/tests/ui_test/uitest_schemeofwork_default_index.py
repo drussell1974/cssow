dac82c236cbbedf6fee5307bc3f33d61857a5c32
drussell1974 <dave@jazzthecat.co.uk>
2021-04-29 06:11:23 +0100

    remove redundant test for checking latest schemes of work view (#478)

--- src/teacher_web/tests/ui_test/uitest_schemeofwork_default_index.py
+++ src/teacher_web/tests/ui_test/uitest_schemeofwork_default_index.py
@@ -47,21 +47,6 @@ class uitest_schemeofwork_default_index(UITestCase):
         self.assertEqual(3, result, "number of elements not as expected")
 
 
-    def test_page__has_show_published_and_owned_latest_schemesofwork(self):
-        # setup
-        
-        # act
-
-        section = self.test_context.find_elements_by_class_name('post-preview--schemeofwork')
-
-        result = len(section)
-
-        # assert
-        # ***** less 5 should be visible to test@localhost for testing purposes
-        ''' TEMPORARILY SET TO 4 SHOULD BE 3 '''
-        self.assertEqual(4, result, "number of elements not as expected")
-
-
     def test_page__show_institutes(self):
         # setup
         
