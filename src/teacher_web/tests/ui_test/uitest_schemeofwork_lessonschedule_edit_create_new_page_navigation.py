b86f2c36879228b4192ba521d488089a1867d590
drussell1974 <dave@jazzthecat.co.uk>
2021-05-01 11:14:36 +0100

    test lesson schedule pages (#477)

--- src/teacher_web/tests/ui_test/uitest_schemeofwork_lessonschedule_edit_create_new_page_navigation.py
+++ src/teacher_web/tests/ui_test/uitest_schemeofwork_lessonschedule_edit_create_new_page_navigation.py
@@ -29,58 +29,8 @@ class uitest_schemeofwork_lessonschedule_edit_create_new_page_navigation(UITestC
         # assert
         self.assertWebPageTitleAndHeadings('Dave Russell - Teach Computer Science', 'Types of CPU architecture', 'Create schedule for Types of CPU architecture')
         self.assertFooterContextText("Computer Science Finibus Bonorum et Malorum")
-
-
-    """ Breadcrumb """
-
-
-    def test_page__breadcrumb__navigate_to_schemesofwork_index(self):
-        # setup
-        elem = self.test_context.find_element_by_id('btn-bc-schemes_of_work')
-        self.assertEqual("Schemes of Work", elem.text)
-
-        # test
-        elem.click()
-        self.wait()
-
-        # assert
-        self.assertWebPageTitleAndHeadings('Dave Russell - Teach Computer Science', 'Schemes of Work', 'Our shared schemes of work by key stage')
-
-
-    def test_page__breadcrumb__navigate_to_schemesofwork_schedule(self):
-        # setup
-        elem = self.test_context.find_element_by_id('lnk-bc-schemeofwork_schedule')
-        self.assertEqual("Schedule", elem.text)
-
-        # test
-        elem.click()
-        self.wait()
-
-        # assert
-        self.assertWebPageTitleAndHeadings('Dave Russell - Teach Computer Science', 'A-Level Computer Science', 'Scheduled lessons')
-
-
-    def test_page__breadcrumb__navigate_to_lesson_index(self):
-        #test
-        elem = self.test_context.find_element_by_id('lnk-bc-lessons')
-        self.assertEqual("Lessons", elem.text)
-
-        # test
-        elem.click()
-        self.wait()
-
-        # assert
-        self.assertWebPageTitleAndHeadings('Dave Russell - Teach Computer Science', 'A-Level Computer Science', 'Lessons')
-
-
-    def test_page__breadcrumb__navigate_to_lesson_schedule(self):
-        #test
-        elem = self.test_context.find_element_by_id('lnk-bc-lesson_schedule')
-        self.assertEqual("Schedule", elem.text)
-
-        # test
-        elem.click()
-        self.wait()
-
-        # assert
-        self.assertWebPageTitleAndHeadings('Dave Russell - Teach Computer Science', 'Types of CPU architecture', 'Scheduled lessons')
+        self.assertTopNavShouldHaveHomeIndex(True)
+        self.assertTopNavShouldHaveDepartmentsIndex(False)
+        self.assertBreadcrumbShouldHaveDepartmentsIndex(True)
+        self.assertBreadcrumbShouldHaveSchemesOfWorkIndex(True)
+        self.assertBreadcrumbShouldHaveLessonsIndex(True)
--- src/teacher_web/tests/ui_test/uitest_schemeofwork_lessonschedule_index.py
+++ src/teacher_web/tests/ui_test/uitest_schemeofwork_lessonschedule_index.py
@@ -25,6 +25,11 @@ class uitest_schemeofwork_lessonschedule_index(UITestCase):
         # assert
         self.assertWebPageTitleAndHeadings('Dave Russell - Teach Computer Science', 'Types of CPU architecture', 'Scheduled lessons')
         self.assertFooterContextText("Computer Science Finibus Bonorum et Malorum")
+        self.assertTopNavShouldHaveHomeIndex(True)
+        self.assertTopNavShouldHaveDepartmentsIndex(False)
+        self.assertBreadcrumbShouldHaveDepartmentsIndex(True)
+        self.assertBreadcrumbShouldHaveSchemesOfWorkIndex(True)
+        self.assertBreadcrumbShouldHaveLessonsIndex(True)
 
 
     def test_page__should_have__sidebar_and_selected_lesson(self):
@@ -34,31 +39,9 @@ class uitest_schemeofwork_lessonschedule_index(UITestCase):
         # assert
         self.assertEqual("Types of CPU architecture", elem.text)
         self.assertEqual("nav-link", elem.get_attribute("class"))
-
-
-    def test_page__should_have__group_heading(self):
-        # test
         elem = self.test_context.find_element_by_class_name('group-heading')
-
-        # assert
         self.assertEqual("Schedule 2020/2021", elem.text)
 
-
-    def test_page__breadcrumb__navigate_to_schemesofwork_index(self):
-        # setup
-        self.test_context.find_element_by_id('lnk-bc-schemes_of_work').click()
-
-        # assert
-        self.assertWebPageTitleAndHeadings('Dave Russell - Teach Computer Science', 'Schemes of Work', 'Our shared schemes of work by key stage')
-
-
-    def test_page__breadcrumb__navigate_to_lessons_index(self):
-        # setup
-
-        self.test_context.find_element_by_id('lnk-bc-lessons').click()
-
-        # assert
-        self.assertWebPageTitleAndHeadings('Dave Russell - Teach Computer Science', 'A-Level Computer Science', 'Lessons')
 
     @skip("needs static events")
     def test_page__show_published_only(self):
