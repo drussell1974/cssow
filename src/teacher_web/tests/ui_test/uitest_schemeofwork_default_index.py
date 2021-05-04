ec7097732db5d10a860cdfcaf03db250f64c8277
drussell1974 <dave@jazzthecat.co.uk>
2021-05-04 04:18:10 +0100

    update test to include scheme of work without topics (#483)

--- src/teacher_web/tests/ui_test/uitest_schemeofwork_default_index.py
+++ src/teacher_web/tests/ui_test/uitest_schemeofwork_default_index.py
@@ -49,7 +49,7 @@ class uitest_schemeofwork_default_index(UITestCase):
 
         # assert
         # ***** less 5 should be visible to test@localhost for testing purposes
-        self.assertEqual(3, result, "number of elements not as expected")
+        self.assertEqual(4, result, "number of elements not as expected")
 
 
     def test_page__show_institutes(self):
