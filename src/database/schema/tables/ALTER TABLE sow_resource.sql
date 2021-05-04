0ccd2937f87b8325579daf53be8a724600ad02a7
drussell1974 <dave@jazzthecat.co.uk>
2021-05-04 05:12:07 +0100

    update foreign keys to cascade on delete (#479)

--- src/database/schema/tables/ALTER TABLE sow_resource.sql
+++ src/database/schema/tables/ALTER TABLE sow_resource.sql
@@ -1,7 +1,7 @@
 SELECT * FROM drussell1974$cssow_api.sow_resource WHERE lesson_id NOT IN (SELECT id FROM sow_lesson);
 
 ALTER TABLE `drussell1974$cssow_api`.`sow_resource` 
-ADD INDEX `fk_sow_resource__has__lesson_idx` (`lesson_id` ASC) VISIBLE;
+ADD INDEX `fk_sow_resource__has__lesson_idx` (`lesson_id` ASC);
 ;
 ALTER TABLE `drussell1974$cssow_api`.`sow_resource` 
 ADD CONSTRAINT `fk_sow_resource__has__lesson`
--- src/database/schema/tables/alter_table__sow_lesson.sql
+++ src/database/schema/tables/alter_table__sow_lesson.sql
@@ -1,2 +1,8 @@
-ALTER TABLE sow_lesson ADD IF NOT EXISTS content_id int(11) NOT NULL default (0) after scheme_of_work_id;
-
+ALTER TABLE `sow_lesson` 
+DROP FOREIGN KEY `sow_learning_episode__has__year_id`;
+ALTER TABLE `drussell1974$cssow_api`.`sow_lesson` 
+ADD CONSTRAINT `sow_lesson__has__year_id`
+  FOREIGN KEY (`year_id`)
+  REFERENCES `drussell1974$cssow_api`.`sow_year` (`id`)
+  ON DELETE CASCADE
+  ON UPDATE RESTRICT;
